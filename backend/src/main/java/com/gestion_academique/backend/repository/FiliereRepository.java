81740f83e86f8d1ff48fd22be43cc08c72880f77
Zeineb <darouaz.zeineb@gmail.com>
2026-04-19 22:20:20 +0200

    feat:Add FiliereRepository

---
+++ backend/src/main/java/com/gestion_academique/backend/repository/FiliereRepository.java
@@ -0,0 +1,7 @@
+package com.gestion_academique.backend.repository;
+
+import com.gestion_academique.backend.entity.Filiere;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FiliereRepository extends JpaRepository<Filiere, Long> {
+}
